d27e47fcef1695e3728f2ea3ae3d0f1d7c361c78
seong yeong jun <75199294+tddudwns1@users.noreply.github.com>
2023-07-25 09:05:49 +0900

    Update and rename BOJ1202.java to BOJ1012.java

--- src/성영준/BOJ1012.java
+++ src/성영준/BOJ1012.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class BOJ1202 {
+class BOJ1012 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
