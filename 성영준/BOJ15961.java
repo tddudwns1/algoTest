661b83c25560cc4c0403353cdabf6eb6e8efb493
seong yeong jun <tjddudwns1@naver.com>
2023-08-25 08:17:12 +0900

    23/08/25 성영준 g4 BOJ15961

---
+++ 성영준/BOJ15961.java
@@ -0,0 +1,51 @@
+package 성영준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15961 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 접시 수
+        int d = Integer.parseInt(st.nextToken()); // 초밥 종류
+        int k = Integer.parseInt(st.nextToken()); // 연속
+        int c = Integer.parseInt(st.nextToken()); // 쿠폰
+
+        int cnt = 1;
+        int[] counts = new int[d + 1];
+        int[] set = new int[k];
+        int[] setFirst = new int[k];
+
+        counts[c] = 1;
+        int i = 0;
+        for (; i < k; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (counts[input]++ == 0)
+                cnt++;
+            set[i] = setFirst[i] = input;
+        }
+        int max = cnt;
+        for (; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (--counts[set[i % k]] == 0)
+                cnt--;
+            set[i % k] = input;
+            if (counts[input]++ == 0)
+                max = Math.max(max, ++cnt);
+        }
+        for (int j = 0; j < k - 1; j++) {
+            int input = setFirst[j];
+            if (--counts[set[i % k]] == 0)
+                cnt--;
+            set[i++ % k] = input;
+            if (counts[input]++ == 0)
+                max = Math.max(max, ++cnt);
+        }
+
+        System.out.println(max);
+    }
+}
