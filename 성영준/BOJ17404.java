b3b3539763e320dd324579c0e14af71e05d0f892
seong yeong jun <tjddudwns1@naver.com>
2023-08-26 21:58:41 +0900

    23/08/26 성영준 g4 BOJ17404

---
+++ 성영준/BOJ17404.java
@@ -0,0 +1,39 @@
+package 성영준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ17404 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] house = new int[n--][9];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 9; i++)
+            house[0][i] = 1001;
+        for (int i = 0; i < 3; i++)
+            house[0][i * 4] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int left = Integer.parseInt(st.nextToken());
+            int center = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 3; j++) {
+                int line = j;
+                int line2 = line + 1;
+                int line3 = line + 2;
+                house[i + 1][line] = Math.min(house[i][line2], house[i][line3]) + left;
+                house[i + 1][line2] = Math.min(house[i][line], house[i][line3]) + center;
+                house[i + 1][line3] = Math.min(house[i][line], house[i][line2]) + right;
+            }
+        }
+        for (int i = 0; i < 3; i++)
+            house[n][i * 4] = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < 9; i++)
+            ans = Math.min(ans, house[n][i]);
+        System.out.println(ans);
+    }
+}
