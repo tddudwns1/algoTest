8f1dffa66ff7b7d147874024d2aca6bbfe9673b2
seong yeong jun <tjddudwns1@naver.com>
2023-08-30 21:36:04 +0900

    23/08/30 성영준 s2 BOJ10971

---
+++ 성영준/BOJ10971.java
@@ -0,0 +1,44 @@
+package 성영준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10971 {
+    static int min = Integer.MAX_VALUE, n;
+    static int[][] costs;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        costs = new int[n][n];
+
+        visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++)
+                costs[i][j] = Integer.parseInt(st.nextToken());
+        }
+        visited[0] = true;
+        dfs(0, 0, 0, n - 1);
+        System.out.println(min);
+    }
+
+    private static void dfs(int startCity, int nowCity, int nowCosts, int dep) {
+        if (dep == 0) {
+            if (costs[nowCity][startCity] != 0)
+                min = Math.min(min, nowCosts + costs[nowCity][startCity]);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && costs[nowCity][i] != 0 && nowCosts + costs[nowCity][i] < min) {
+                visited[i] = true;
+                dfs(startCity, i, nowCosts + costs[nowCity][i], dep - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
